bd492d8f4a0e72d4ec2853d478074717011a95b8
yedoka <akedilbekbolatov@gmail.com>
2025-01-22 13:56:06 +0100

    removed unused imports

--- app/src/main/java/com/example/myapplication2/RecipesViewModel.kt
+++ app/src/main/java/com/example/myapplication2/RecipesViewModel.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class RecipesViewModel : ViewModel() {
